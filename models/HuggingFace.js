9a9ae960ac887c89f1b21b025656cb7b8e720754
Kelton Temby <kelstock@gmail.com>
2022-11-29 01:31:00 -0800

    initial HF API sample

--- models/HuggingFace.js
+++ models/HuggingFace.js
@@ -3,6 +3,24 @@ import { HUGGINGFACE_API_KEY } from '@env'
 
 const url = 'https://api.openai.com/v1/images/generations'
 
+
+async function query(data) {
+    const response = await fetch(
+        "https://api-inference.huggingface.co/models/gpt2",
+        {
+            headers: { Authorization: `Bearer ${API_TOKEN}` },
+            method: "POST",
+            body: JSON.stringify(data),
+        }
+    );
+    const result = await response.json();
+    return result;
+}
+query("Can you please let us know more details about your ").then((response) => {
+    console.log(JSON.stringify(response));
+});
+// [{"generated_text":"Can you please let us know more details about your ids as a subscriber or other related project? Be sure to update your username and password or it will be stolen via email. Our information is only accessible through our website, and the payment support services"}]
+
 function requestImageJson(imagePrompt, n_images) {
   return ( {
     method: 'POST',
