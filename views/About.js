e117917b7de37765526597b6cefe732ebeb60859
Kelton Temby <kelstock@gmail.com>
2022-12-15 13:56:00 -0800

    removes zoomies and story creation from account screen

--- views/About.js
+++ views/About.js
@@ -12,17 +12,10 @@ function AboutScreen({navigation}) {
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Text style = {styles.SubHeading}>The Adventures of</Text>
             <Text style = {styles.HeadingAlt}>Copernicus and Margot</Text>
-            <Pressable onPress={() => navigation.navigate('Zoomies!')} >
-              <Image style={styles.ImageStyle}
-            		source = {require( imageString ) }>
-              </Image>
-            </Pressable>
+            <Image style={styles.ImageStyle}
+          		source = {require( imageString ) }>
+            </Image>
             <Text style={{color: 'white', paddingTop: 10}}>Curated AI stories and Art</Text>
-            <Pressable onPress={() => navigation.navigate('Create')} >
-              <View style={{backgroundColor: '#424242AA', marginTop: 30, borderRadius: 5 }}>
-              <Text style={{color: 'white', fontWeight: 'bold', padding: 10}}>Create a Story!</Text>
-              </View>
-            </Pressable>
             <Pressable onPress={() => navigation.navigate('CatCreation')} >
               <View style={{backgroundColor: '#424242AA', marginTop: 30, borderRadius: 5 }}>
               <Text style={{color: 'white', fontWeight: 'bold', padding: 10}}>See My Cats</Text>
