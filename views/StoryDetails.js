977a4d4d0cf1cc6b2cca06c98580753a72dbba31
Kelton Temby <kelstock@gmail.com>
2023-01-27 01:06:39 -0800

    enables story views to read from library rather than static json

--- views/StoryDetails.js
+++ views/StoryDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styles, {getColorScheme} from '../views/Styles';
 import StoryViewer from '../components/StoryViewer';
@@ -7,9 +7,12 @@ import StoryViewer from '../components/StoryViewer';
 function StoryDetail({ route, navigation}) {
   const { item } = route.params;
   const themeColorStyle = getColorScheme();
-  const myCDN = "https://d2sphvb6m6942c.cloudfront.net/";
 
-  let thisStory = new StoryViewer(item.name, myCDN.concat(item.image).replace(/ /g, "%20"), item.description);
+  let prepend = "";
+  const myCDN = "https://d2sphvb6m6942c.cloudfront.net/";
+  !!item.cdn ? prepend = myCDN : "";
+
+  let thisStory = new StoryViewer(item.name, prepend.concat(item.image).replace(/ /g, "%20"), item.description);
 
   return (
       <SafeAreaView style={[styles.safeAreaFull, themeColorStyle]}>
