f97477ee6b9d760f732b79fb5db3f9b894bce956
Kelton Temby <kelstock@gmail.com>
2023-02-08 12:48:36 -0800

    fix: balance renders better on smaller phones

--- components/Balance.js
+++ components/Balance.js
@@ -6,13 +6,13 @@ let Balance = (props) => {
   const themeColorStyle = getColorScheme();
 
   return(
-    <View style={{width: "100%", height: 34, position: 'absolute', right: 0, top: 59, left: "75%", backgroundColor:"#21212122", borderRadius: 15}}>
-      <View style={{flex: 1, flexDirection: 'row'}}>
-        <View style={{width: "5%", justifyContent: "center", alignItems: "flex-end", marginLeft: 10}} >
+    <View style={{width: "30%", height: 34, position: 'absolute', right: 0, top: 59, backgroundColor:"#21212122", borderRadius: 15}}>
+      <View style={{flex: 1, flexDirection: 'row', marginLeft: 3}}>
+        <View style={{width: "20%", justifyContent: "center", alignItems: "flex-end"}} >
           <Coin size={20} />
         </View>
-        <View style={{width: "20%", justifyContent: "center", alignItems: "flex-start"}} >
-          <Text style={[styles.body, {color: themeColorStyle.color, margin: 0, marginLeft: 3}]}>14500</Text>
+        <View style={{width: "80%", justifyContent: "center", alignItems: "flex-start", marginLeft: 3}} >
+          <Text style={[styles.buttonTextStyle, {color: themeColorStyle.color, margin: 0}]}>14500</Text>
         </View>
       </View>
     </View>
