eaff504103d0be029321e9b753c3f56edcdec9d2
Kelton Temby <kelstock@gmail.com>
2023-02-07 22:31:14 -0800

    feat: uses cached image for cat viewing

--- views/CatCreation.js
+++ views/CatCreation.js
@@ -4,6 +4,7 @@ import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context'
 import { LinearGradient } from "expo-linear-gradient";
 import styles, {getColorScheme} from '../views/Styles';
 import Cat, {copernicusValues, margotValues} from "../models/Cat";
+import CachedImage from "../components/CachedImage";
 
 function renderCatCard(thisCat) {
   const themeColorStyle = getColorScheme();
@@ -16,7 +17,7 @@ function renderCatCard(thisCat) {
           <Text style = {[styles.Heading, themeColorStyle, {marginBottom: 5}]} >{thisCat.state.name}</Text>
           <Text style = {[styles.SubHeading, themeColorStyle, { marginBottom: 20}]} >{thisCat.state.title}</Text>
 
-            <Image source={{uri: thisCat.state.image}}  style={[styles.ImageStyle, {borderRadius: 10, width: '80%', flex: 1, margin: 10}]}></Image>
+            <CachedImage source={{uri: thisCat.state.image}}  style={[styles.ImageStyle, {borderRadius: 10, width: '80%', flex: 1, margin: 10}]}></CachedImage>
 
           <Text style = {[styles.body, themeColorStyle, {textAlign: 'center'}]} >{thisCat.catText()}</Text>
 
@@ -49,3 +50,17 @@ function CatCreation({navigation}) {
 }
 
 export default CatCreation;
+
+/* Blurhash as a container working on iOS not android
+import { Blurhash } from 'react-native-blurhash';
+const blurhash = await Blurhash.encode('https://blurha.sh/assets/images/img2.jpg', 4, 3)
+<View style={[styles.container, {width: "100%"}]}>
+  <Blurhash
+    blurhash="LGFFaXYk^6#M@-5c,1J5@[or[Q6."
+    style={{flex: 1, width: "100%", height: 100, justifyContent: "center", alignItems: "center"}}>
+  </Blurhash>
+  <Text style={[styles.Heading, themeColorStyle]}>Buy Coins!</Text>
+  <Text style={[styles.text, {textAlign: "center"}, themeColorStyle]}>for more stories</Text>
+</View>
+
+*/
