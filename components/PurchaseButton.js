613b077ec04e5d43a7fe91627eb1a16ba6ac5cdc
Kelton Temby <kelstock@gmail.com>
2023-02-08 22:23:18 -0800

    refactor: using props object

--- components/PurchaseButton.js
+++ components/PurchaseButton.js
@@ -3,23 +3,23 @@ import styles from '../views/Styles';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Coin from '../components/Coin';
 
-let PurchaseButton = (callback, text, price, icon) => {
+let PurchaseButton = (props) => {
   return(
     <View style={{flex: 1, width: "70%", margin: 10}}>
-      <Pressable onPress={callback} >
+      <Pressable onPress={props.callback} >
         <View style={styles.buttonContainerStyle}>
 
           <View style={{flex: 1, flexDirection: 'row'}}>
             <View style={{width: "70%", justifyContent: "center"}} >
-              <Text style={styles.buttonTextStyle}>{text}</Text>
+              <Text style={styles.buttonTextStyle}>{props.title}</Text>
             </View>
             <View style={{width: "10%", justifyContent: "center", alignItems: "flex-end"}} >
-              {!!icon &&
+              {!!props.icon &&
                 <Coin size={20} />
               }
             </View>
             <View style={{width: "20%", justifyContent: "center", alignItems: "flex-start"}}>
-              <Text style={[styles.buttonTextStyle, {marginLeft: 3}]}>{price}</Text>
+              <Text style={[styles.buttonTextStyle, {marginLeft: 3}]}>{props.price}</Text>
             </View>
           </View>
 
