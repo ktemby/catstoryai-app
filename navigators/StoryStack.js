a9532665d0f0484d72e1c0781785b55bb9609705
Kelton Temby <kelstock@gmail.com>
2023-02-15 23:45:49 -0800

    feat: enables nav to store from story stack, and reformat with prettier

--- navigators/StoryStack.js
+++ navigators/StoryStack.js
@@ -1,23 +1,50 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import StoriesScreen from '../views/StoryList';
-import StoryDetail from '../views/StoryDetails';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import StoriesScreen from "../views/StoryList";
+import StoryDetail from "../views/StoryDetails";
+import Store from "../views/Store";
+import { AppContext } from "../store/context";
+import CreateStory from "../views/CreateStory";
 
 const Stack = createNativeStackNavigator();
 
 function StoryStack() {
+  const { themeColorStyle } = React.useContext(AppContext);
+
   return (
-      <Stack.Navigator>
-        <Stack.Screen name="Stories" component={StoriesScreen}
-          options={{
-            headerShown:false
-          }}
-        />
-        <Stack.Screen name="Story Detail" component={StoryDetail}
-          options={{
-            headerShown:false,
-          }}
-        />
-      </Stack.Navigator>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Stories"
+        component={StoriesScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Story Detail"
+        component={StoryDetail}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Store"
+        component={Store}
+        options={{
+          title: "Store",
+          headerTransparent: true,
+          headerTintColor: themeColorStyle.color,
+        }}
+      />
+      <Stack.Screen
+        name="Create"
+        component={CreateStory}
+        options={{
+          title: "Create",
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
   );
 }
 
