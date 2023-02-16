ada1d396c44fa4caba7afd9b634d6a19bf58e2de
Kelton Temby <kelstock@gmail.com>
2023-02-15 23:46:17 -0800

    reformat: prettier

--- store/context.js
+++ store/context.js
@@ -1,37 +1,40 @@
-import { createContext, useState, useEffect } from 'react';
-import { useColorScheme } from 'react-native';
-import { MyDarkTheme, MyLightTheme} from "../views/Styles";
+import { createContext, useState, useEffect } from "react";
+import { useColorScheme } from "react-native";
+import { MyDarkTheme, MyLightTheme } from "../views/Styles";
 
 const AppContext = createContext();
 
-const AppContextProvider = ({children}) => {
+const AppContextProvider = ({ children }) => {
   const colorScheme = useColorScheme();
   const [darkThemeOverride, setDarkThemeOverride] = useState(false);
-  // instead of true, would be idea to get this state directly from the settings storage object.
+  // instead of true, would be ideal to get this state directly from the settings storage object.
 
-  const [themeColorStyle, setThemeColorStyle] = useState(darkThemeOverride === true ? MyDarkTheme : ((colorScheme === 'dark') ? MyDarkTheme : MyLightTheme));
+  const [themeColorStyle, setThemeColorStyle] = useState(
+    darkThemeOverride === true
+      ? MyDarkTheme
+      : colorScheme === "dark"
+      ? MyDarkTheme
+      : MyLightTheme
+  );
 
-  let context = {darkThemeOverride, setDarkThemeOverride, themeColorStyle, setThemeColorStyle};
+  let context = {
+    darkThemeOverride,
+    setDarkThemeOverride,
+    themeColorStyle,
+    setThemeColorStyle,
+  };
 
   useEffect(() => {
-    //setTheme(theme === 'auto' ? colorScheme : 'dark');
-    setThemeColorStyle(darkThemeOverride === true ? MyDarkTheme : ((colorScheme === 'dark') ? MyDarkTheme : MyLightTheme));
+    setThemeColorStyle(
+      darkThemeOverride === true
+        ? MyDarkTheme
+        : colorScheme === "dark"
+        ? MyDarkTheme
+        : MyLightTheme
+    );
   }, [colorScheme, darkThemeOverride, themeColorStyle]);
 
-    return (
-        <AppContext.Provider value={context}>
-            {children}
-        </AppContext.Provider>
-    )
-}
+  return <AppContext.Provider value={context}>{children}</AppContext.Provider>;
+};
 
-export {
-    AppContext,
-    AppContextProvider
-}
-
-
-/*
-setThemeColorStyle( (value === true) ? MyDarkTheme
-  : (colorScheme === 'dark') ? MyDarkTheme : MyLightTheme);
-*/
+export { AppContext, AppContextProvider };
