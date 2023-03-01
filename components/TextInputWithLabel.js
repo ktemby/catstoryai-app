dbec987be18e369019dcaf32bd2c88441f112ef4
Kelton Temby <kelstock@gmail.com>
2023-03-01 01:44:48 -0800

    feat: enables styling as props for flexibility

--- components/TextInputWithLabel.js
+++ components/TextInputWithLabel.js
@@ -5,30 +5,27 @@ import { AppContext } from "../store/context";
 
 const TextInputWithLabel = (props) => {
   const { themeColorStyle } = useContext(AppContext);
-
   return (
-    <View style={[styles.inputWrapper, themeColorStyle]}>
+    <View style={[styles.inputWrapper, props.frameStyle, themeColorStyle]}>
       <View style={[themeColorStyle, { width: "90%" }]}>
         <Text
-          style={[
-            { color: "#212121", fontWeight: "bold", marginBottom: 10 },
-            themeColorStyle,
-          ]}
+          style={[{ fontWeight: "bold", marginBottom: 10 }, themeColorStyle]}
         >
           {props.label}
         </Text>
         <TextInput
           placeholder={props.placeholder}
+          autoCapitalize={props.autoCapitalize}
           placeholderTextColor="#9E9E9E"
-          onChangeText={(text) => props.setParentInput(text)}
-          value={props.parentInput}
+          onChangeText={props.onChangeText}
+          value={props.value}
           style={[
             {
               fontSize: 18,
             },
             themeColorStyle,
           ]}
-          multiline={true}
+          multiline={props.multiline === undefined ? true : props.multiline}
           autoFocus={true}
         />
       </View>
