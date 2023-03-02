66518e461dcc5f44bc3b333353960c9a02ad3e0b
Kelton Temby <kelstock@gmail.com>
2023-03-02 12:39:00 -0800

    chore: prettier reformat

--- components/StoryViewerButtons.js
+++ components/StoryViewerButtons.js
@@ -1,6 +1,7 @@
-import { View, Pressable} from 'react-native';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import styles, {getColorScheme} from '../views/Styles';
+import { useState } from "react";
+import { View, Pressable } from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import styles from "../views/Styles";
 
 // Todo, refactor these and set up proper callbacks if needed.
 
@@ -10,36 +11,49 @@ export let FavoriteButton = (props) => {
 
   return (
     <View>
-     <Pressable
-       onPressIn={() => setColor("#424242")}
-       onPressOut={() => setColor(null)}
-       onPress={() => setFav(!fav)}
-       style={[props.style, { backgroundColor: backColor}]}
-     >
-      <MaterialCommunityIcons name="star" color={fav ? 'yellow' : '#616161'} size={50} />
+      <Pressable
+        onPressIn={() => setColor("#424242")}
+        onPressOut={() => setColor(null)}
+        onPress={() => {
+          setFav(!fav);
+          props.onPress;
+        }}
+        style={[props.style, { backgroundColor: backColor }]}
+      >
+        <MaterialCommunityIcons
+          name="star"
+          color={fav ? "yellow" : "#616161"}
+          size={50}
+        />
       </Pressable>
     </View>
- ); //props.item.isFavorite = !props.item.isFavorite
-}
+  ); //props.item.isFavorite = !props.item.isFavorite
+};
 
 export let DeleteButton = (props) => {
   let [backColor, setColor] = useState(null);
   let [binColor, setBinColor] = useState("#616161");
 
   return (
-    <View >
-     <Pressable
-      style={[props.style, { backgroundColor: backColor}]}
-      onPressIn={() => {setColor("#424242"), setBinColor("#FFF")}}
-      onPressOut={() => {setColor(null), setBinColor("#616161")}}
-      onPress={() => alert(`Delete ${props.item.name}?`)}
-     >
-      <MaterialCommunityIcons name="delete" color={binColor} size={50} />
+    <View>
+      <Pressable
+        style={[props.style, { backgroundColor: backColor }]}
+        onPressIn={() => {
+          setColor("#424242"), setBinColor("#FFF");
+        }}
+        onPressOut={() => {
+          setColor(null), setBinColor("#616161");
+        }}
+        onPress={() => {
+          alert(`Delete ${props.item.name}?`);
+          props.onPress;
+        }}
+      >
+        <MaterialCommunityIcons name="delete" color={binColor} size={50} />
       </Pressable>
     </View>
- );
-}
-
+  );
+};
 
 /*
 Inside Story StoryViewer
